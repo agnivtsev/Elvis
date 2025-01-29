94e0004c64a60ae77c3589c9eec751aaad8e9bc3
Alexander Agnivtsev <agnivtsev@gmail.com>
2025-01-30 00:31:56 +0300

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Alexander Agnivtsev on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
